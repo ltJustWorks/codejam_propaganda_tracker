3a1df123dbe86c9b2b0a0847ed2df0bfc2f90802
ltJustWorks <43526682+ltJustWorks@users.noreply.github.com>
2023-11-18 11:52:24 -0500

    get search to work with wikipedia

---
+++ gpt.py
@@ -0,0 +1,21 @@
+import openai
+import os
+
+from openai import OpenAI
+
+client = OpenAI(api_key="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")
+# defaults to getting the key using os.environ.get("OPENAI_API_KEY")
+# if you saved the key under a different environment variable name, you can do something like:
+# client = OpenAI(
+#   api_key=os.environ.get("CUSTOM_ENV_NAME"),
+# )
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
