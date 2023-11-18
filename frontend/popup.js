3f95088de4c391a17bdd04a61ba95cb9f7d3bf6e
aliceg01 <alice.godbout@mail.mcgill.ca>
2023-11-18 14:09:17 -0500

    updated html and css

--- frontend/popup.js
+++ frontend/popup.js
@@ -1,8 +1,8 @@
-// This script could interact with content.js or background.js
-document.addEventListener('DOMContentLoaded', function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { fromPopup: true }, function (response) {
-            document.getElementById('result').innerText = response.factCheckSummary;
-        });
-    });
-});
+// // This script could interact with content.js or background.js
+// document.addEventListener('DOMContentLoaded', function () {
+//     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//         chrome.tabs.sendMessage(tabs[0].id, { fromPopup: true }, function (response) {
+//             document.getElementById('result').innerText = response.factCheckSummary;
+//         });
+//     });
+// });
