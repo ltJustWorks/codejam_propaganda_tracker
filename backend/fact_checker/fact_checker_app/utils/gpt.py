00fc721420f2ea170653b69d8fc961b3a4390ea4
aliceg01 <alice.godbout@mail.mcgill.ca>
2023-11-18 23:41:41 -0500

    update

--- backend/fact_checker/fact_checker_app/utils/gpt.py
+++ backend/fact_checker/fact_checker_app/utils/gpt.py
@@ -1,12 +1,10 @@
 import json
-import openai
-import os
 
 from openai import OpenAI
 
 client = OpenAI(
     # defaults to os.environ.get("OPENAI_API_KEY")
-    #api_key="My API Key",
+    # api_key="My API Key",
 )
 # defaults to getting the key using os.environ.get("OPENAI_API_KEY")
 # if you saved the key under a different environment variable name, you can do something like:
@@ -14,21 +12,33 @@ client = OpenAI(
 #   api_key=os.environ.get("CUSTOM_ENV_NAME"),
 # )
 
+
 def get_prompt(tweet, summary):
-    return """You are given background information.
-    Information: """ + summary + "\n" + """
+    return (
+        """You are given background information.
+    Information: """
+        + summary
+        + "\n"
+        + """
     Does the following tweet seem to be truthful, based on what is observed in the background information?
-    Tweet: """ + tweet + """
+    Tweet: """
+        + tweet
+        + """
     Explain your reasoning.
 """
+    )
+
 
 def fact_check(tweet, data_dict):
     result = client.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages=[
-      {"role": "system", "content": "You are a fact-checker that will check tweets on Twitter and ensure their truthfulness."},
-      {"role": "user", "content": get_prompt(tweet, data_dict["summary"])}
-      ]
+        model="gpt-3.5-turbo",
+        messages=[
+            {
+                "role": "system",
+                "content": "You are a fact-checker that will check tweets on Twitter and ensure their truthfulness.",
+            },
+            {"role": "user", "content": get_prompt(tweet, data_dict["summary"])},
+        ],
     )
     json_result = result.choices[0].message.model_dump_json(indent=2)
     return json.loads(json_result)
--- fact-checker-chrome-extension/src/App.js
+++ fact-checker-chrome-extension/src/App.js
@@ -1,14 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const DJANGO_ENDPOINT = "http://127.0.0.1:8000"
+
+const handleInputChange = (newInput, setInput) => {
+  setInput(newInput);
+}
 
 function App() {
+  const [input, setInput] = useState("")
+  const [response, setResponse] = useState(null)
+  const [inputSubmitted, setInputSubmitted] = useState(false)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(DJANGO_ENDPOINT + "fact-check/?input_string=" + input)
+        setResponse(response)
+      }
+      catch (e) {
+        console.error("error while getting api response:", e)
+      }
+    }
+
+    fetchData()
+    //setInputSubmitted(false)
+  }, [inputSubmitted])
+
   return (
     <div className="App">
-      <div>
+      <div className="title">
         Fact Checker
       </div>
+      <div className="content-fields">
+        <input type='text' placeholder='Type text here' onChange={(newInput) => handleInputChange(newInput, setInput)} />
+        <button onClick={() => setInputSubmitted(true)}>Check</button>
+      </div>
       <div>
-        Check Button
+        {response ? (<p>Response: {JSON.stringify(response, null, 2)}</p>) : null}
       </div>
     </div>
   );
