a15d126f5373adda29376c45f2f25824f65ea2a5
aliceg01 <alice.godbout@mail.mcgill.ca>
2023-11-19 11:59:08 -0500

    dfsadsa

--- fact-checker-chrome-extension/src/VerdictSymbol.js
+++ fact-checker-chrome-extension/src/VerdictSymbol.js
@@ -2,7 +2,7 @@ function VerdictSymbol({ verdict }) {
     const style = {
         TRUTHFUL: { color: 'green', fontWeight: 'bold' },
         AMBIGUOUS: { color: 'orange', fontWeight: 'bold' },
-        UNTRUTHFUL: { color: 'red', fontWeight: 'bold' }
+        MISLEADING: { color: 'red', fontWeight: 'bold' }
     }[verdict] || {};
 
     return <p style={style}>{verdict}</p>;
